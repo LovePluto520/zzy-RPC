6b8f91bbee25bfe46681be881acf539e22e20d5f
LovePluto520 <strangeman613911@gmail.com>
2025-07-18 15:51:35 +0800

    第一版:简易版RPC框架开发（修改）

--- example-provider/src/main/java/com/zzy/example/provider/EasyProviderExample.java
+++ example-provider/src/main/java/com/zzy/example/provider/EasyProviderExample.java
@@ -4,14 +4,14 @@ import com.zzy.example.common.service.UserService;
 import com.zzy.zzyrpc.registry.LocalRegistry;
 import com.zzy.zzyrpc.server.HttpServer;
 import com.zzy.zzyrpc.server.VertxHttpServer;
-
+import com.zzy.example.provider.UserServiceImpl;
 /**
  * 简易服务提供者
  */
 public class EasyProviderExample {
     public static void main(String[] args) {
         //注册服务
-        LocalRegistry.register(UserService.class.getName(),UserServiceImpl.class);
+        LocalRegistry.register(UserService.class.getName(), UserServiceImpl.class);
 
         //启动web服务
         HttpServer httpServer=new VertxHttpServer();
